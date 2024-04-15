36af9550e3e038876021b6ed70b9e7c068046dbb
vaibhav <vaibhav.w@mypcot.com>
2024-04-15 10:55:00 +0530

    create release build

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -8,3 +8,21 @@ class Failure extends Equatable {
   @override
   List<Object?> get props => [message];
 }
+
+// class Person {
+//   final String name;
+//   final int age;
+
+//   Person(this.name, this.age);
+
+//   @override
+//   bool operator ==(Object other) =>
+//       identical(this, other) ||
+//       other is Person &&
+//           runtimeType == other.runtimeType &&
+//           name == other.name &&
+//           age == other.age;
+
+//   @override
+//   int get hashCode => name.hashCode ^ age.hashCode;
+// }
